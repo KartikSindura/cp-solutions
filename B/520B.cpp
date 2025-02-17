be0f4fa7e7bcaa5054e4acbb3cd2364431d90a84
KartikSindura <kartik.sindura@gmail.com>
2025-02-18 02:34:00 +0530

    use single file for testcases?

---
+++ B/520B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define lp(i, n) for (int i = 0; i < n; i++)
+#define lps(start, n) for (int i = start; i <= n; i++)
+
+int main() {}
+
+// Think on paper, not on code.
+// Read problem statement twice.
+// Think pictorially.
+
+// Range overlap check => !(b<c || d<a)
+// cout << setprecision(int);
+// cout << num;
+// or cout << fixed << setprecision(int) << num;
+// Look for monotonic behaviour -> binary search
