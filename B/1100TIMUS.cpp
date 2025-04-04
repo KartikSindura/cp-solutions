56ec3bbe62921c378c77e37d62a6a7664ed6164a
KartikSindura <kartik.sindura@gmail.com>
2025-04-04 19:03:31 +0530

    1

--- B/1100TIMUS.cpp
+++ B/1100TIMUS.cpp
@@ -1,10 +1,23 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 #define lp(i, n) for (int i = 0; i < n; i++)
 #define lps(start, n) for (int i = start; i <= n; i++)
 
-int main() {}
+int main() {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> vec(n);
+  lp(i, n) { cin >> vec[i].first >> vec[i].second; }
+  stable_sort(vec.begin(), vec.end(),
+              [](const pair<int, int> a, pair<int, int> b) {
+                return a.second > b.second;
+              });
+  for (auto &[u, v] : vec) {
+    cout << u << " " << v << endl;
+  }
+}
 
 // Range overlap check => !(b<c || d<a)
 // or cout << fixed << setprecision(int) << num;
